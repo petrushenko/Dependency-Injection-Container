1aace9dcc3616930fd1bef991727af90ed16f91e
Igor Petrushenko <igor.petrushenko1@gmail.com>
2019-12-18 17:26:31 +0300

    Added unit tests for library

---
+++ DependencyInjectionContainer.Tests/TestableClasses/ServiceImpl3.cs
@@ -0,0 +1,19 @@
+﻿using DependencyInjectionContainer.Attribute;
+
+namespace DependencyInjectionContainer.Tests.TestableClasses
+{
+    public class ServiceImpl3<T> : IService<T> 
+        where T : IRepository
+    {
+        public T Repository;
+        public ServiceImpl3(T repository)
+        {
+            Repository = repository;
+        }
+
+        public override string ToString()
+        {
+            return "Service IMPL 2 " + Repository;
+        }
+    }
+}
